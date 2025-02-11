9f053624e0d2777f5b97afca6afcf5c45f866065
prasad336 <prasadmanore4@gmail.com>
2025-02-11 11:35:01 +0530

    Update helper.js

--- client/src/helper.js
+++ client/src/helper.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = "http://localhost:8000" || "https://chatters-142n.vercel.app"
 // export const BASE_URL = "https://chatters-u1p6.onrender.com"
